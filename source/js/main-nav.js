ed6c74efa46a48d0a2458ada89498b40f30c7abd
Gross <vardazarov01@mail.ru>
2023-04-07 13:34:01 +0300

    add js to main-menu

---
+++ source/js/main-nav.js
@@ -0,0 +1,19 @@
+const mainNav = document.querySelector('.main-nav');
+const toggle = document.querySelector('.page-header__toggle');
+const headerMenuWrapper = document.querySelector('.page-header__main-menu-wrapper')
+const headerMenu = document.querySelector('.page-header__main-menu');
+
+headerMenu.classList.remove('page-header__main-menu--no-js')
+
+toggle.addEventListener('click', function () {
+  if (mainNav.classList.contains('main-nav--opened')) {
+    mainNav.classList.remove('main-nav--opened')
+    headerMenuWrapper.classList.remove('page-header__main-menu-wrapper--opened-nav')
+    toggle.classList.remove('page-header__toggle--opened')
+  } else {
+    mainNav.classList.add('main-nav--opened')
+    headerMenuWrapper.classList.add('page-header__main-menu-wrapper--opened-nav')
+    toggle.classList.add('page-header__toggle--opened')
+  }
+})
+// source/js/main-nav
